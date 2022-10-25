20f57a9753a4ff42566cafc47a0a11ecfe9fea47
Jason Ceballos <jason.ceballos.jr@gmail.com>
2022-10-24 20:16:05 -0500

    I HATE ADS!

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+        <div id="wrapper">
+          <div class="main">
+            <p><strong>Chaos will begin soon.</strong></p>
+          </div>
+        </div>       
+  );
+}
+
+export default App;
